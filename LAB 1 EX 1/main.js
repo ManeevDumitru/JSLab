10bbde43dfc5dd765431874eabb9db15802e24eb
Maneev Dumitru <54940217+ManeevDumitru@users.noreply.github.com>
2020-01-10 21:07:48 +0200

    Add files via upload

--- LAB 1 EX 1/main.js
+++ LAB 1 EX 1/main.js
@@ -5,18 +5,19 @@
 // 4. Display calculations
 
 
-document.getElementById('btn').addEventListener('click', function() {
-  addMark(Number(document.getElementById('studentsMark').value) ,Number(document.getElementById('mark').value));
+document.getElementById('btn').addEventListener('click', function () {
+  addMark(Number(document.getElementById('studentsMark').value), Number(document.getElementById('mark').value));
   calculations();
+  showResults();
 });
 
 let marks = [];
 
-const addMark = (studentsMark ,mark) => { // adds a mark to the studentsMark array and validates mark
+const addMark = (studentsMark, mark) => { // adds a mark to the studentsMark array and validates mark
   if (studentsMark > marks.length && mark >= 1 && mark <= 10) {
     marks.push(mark);
     console.log(marks);
-    displayResult();
+    showResults();
   }
 };
 
@@ -43,16 +44,30 @@ const calculations = () => { // Calculates everything needed
     difference = max - min;
     media += mark;
   });
-  return `Performance ${Math.round(performance * 100 / marks.length)}%;
-          Quality ${Math.round(quality  * 100 / marks.length)}%;
+  console.log(`Performance ${Math.round(performance * 100 / marks.length)}%;
+          Quality ${Math.round(quality * 100 / marks.length)}%;
           Media ${Math.round(media / marks.length)};
           Difference ${difference};
           Min ${min}; 
-          Max ${max}`
+          Max ${max}`);
+  return [
+    max,
+    min,
+    difference,
+    Math.round(quality * 100 / marks.length),
+    Math.round(performance * 100 / marks.length),
+    Math.round(media / marks.length),
+  ];
 };
 
-const displayResult = () => {
-  document.getElementById('boxForMarks').innerHTML = marks + '<br>' + calculations();
+const showResults = () => {
+  let results = calculations();
+  document.getElementById('marks').innerHTML ='The marks are ' +  marks;
+  document.getElementById('max').innerHTML ='The max mark is ' + results[0];
+  document.getElementById('min').innerHTML ='The minimum marks is ' + results[1];
+  document.getElementById('difference').innerHTML = 'The difference is ' + results[2];
+  document.getElementById('quality').innerHTML ='The quality is ' + results[3] + '%';
+  document.getElementById('performance').innerHTML = 'The performance is ' + results[4] + '%';
+  document.getElementById('media').innerHTML = 'The media is ' + results[5];
 };
-
 
